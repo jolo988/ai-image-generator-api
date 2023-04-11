41b7620524cd15eccee321f1c9a25c24c154aeb2
Jlo <loj1@jlos-air.myfiosgateway.com>
2023-04-11 14:32:09 -0400

    adding heroku DB postgres

--- server.js
+++ server.js
@@ -16,11 +16,10 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password : '',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
