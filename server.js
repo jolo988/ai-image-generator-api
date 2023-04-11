e1f0f2d1d1ca082b06f432b87008464330c5714f
Jlo <loj1@jlos-air.myfiosgateway.com>
2023-04-11 12:37:02 -0400

    adding port

--- server.js
+++ server.js
@@ -2,24 +2,17 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
-// const { Configuration, OpenAIApi } = require('openai');
-// const dotenv = require('dotenv');
-// dotenv.config();
 
+//controllers
 const register = require('./controllers/register');
 const signin = require('./controllers/signin')
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
-// //hide API key
-// const configuration = new Configuration({
-//     apiKey: process.env.OPENAI,
-// });
 
-// const openai = new OpenAIApi(configuration);
 const app = express();
 
-//connect to localhost database
+//postgres Database
 const db = knex({
     client: 'pg',
     connection: {
@@ -31,54 +24,31 @@ const db = knex({
     }
   });
 
-//parse json data from frontend
+//Middleware: parse && cors security
 app.use(express.json());
-app.use(cors()); //apply middleware cors (security mechanism)
+app.use(cors()); 
 
-//res in json format
+//home page end-point
 app.get('/', (req, res) => {
     res.send(db.users);
 });
 
-//need to receive from frontend body via HTTPS
+//Signin end-point
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
-//grab req.body -> enter new info to database
-//pass params req,res,db,bcrypt to handleregister to use, instead of importing to register.js
+//Register end-point
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
-
+//Profile ID end-point
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 
-//Counter
+//Entries/Counter
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 //API call to retrieve image
 app.post('/imagePrompt', (req, res) => { image.handleApiCall(req, res) });
 
-
-
 //host on server #
-app.listen(8081, () => {
-    console.log('app is running on port 8081')
+app.listen(process.env.PORT || 8081, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 });
-
-
-/*
-ROUTES TO WORK ON
-/ --> res = this is working
-
-/signin -> POST / res: success,fail
-(using JSON body to send sensitive data/pw)
-
-/register -> POST / res: return new created user
-(add user info to database or variable in server)
-
-/profile/:userID -> GET / res: return user
-entering /: allows user to enter any number in browser to grab ID via request.params
-(user profile w/ optional ID -> so each user has unique homescreen)
-
-/image -> PUT -> return user or count/score
-(when posting photo, need to change count of submitted photos)
-(PUT bc updated score/count)
-*/
