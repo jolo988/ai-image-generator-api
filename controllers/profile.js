c53871cc3023bfccf7a66d9e54fed25b4dcaee2b
Jonathan Lo <126200054+jolo988@users.noreply.github.com>
2023-04-16 23:29:57 -0400

    Update profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,19 +12,6 @@ const handleProfileGet = async (req, res, db) => {
         }
 }
 
-
-// const handleProfileGet = (req, res, db) => {
-//     const { id } = req.params;
-//     db.select('*').from('users').where({id})
-//         .then(user => {
-//             if (user.length) {
-//                 res.json(user[0])
-//             } else {
-//                 res.status(400).json('Username not found')
-//             }
-//         })
-// }
-
 module.exports = {
     handleProfileGet
 }
